6defca97734c5c22a2f41e387d587e0cf27bd8a1
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-08-27 08:57:26 -0300

    feat: Update Exercicio03.kt - Inclusão do comando da questão, retirada da função imprimirResultado

--- Kotlin-DesafioMentor/Desafio_01/Exercicio03.kt
+++ Kotlin-DesafioMentor/Desafio_01/Exercicio03.kt
@@ -1,40 +1,45 @@
-fun lerDadosDigitados() { 
-  println("Digite o primeiro número: ") 
-  val primeiroNumero: Double = readLine().toString().trim().toDouble()
-  
-  println("Digite o segundo número: ") 
-  val segundoNumero: Double = readLine().toString().trim().toDouble() 
-  
-  println("Digite a operação (+, -, *, /): ") 
-  val operador: String = readLine().toString().trim() 
+/*
+Exercicio 3 - Calculadora Simples
+Crie um programa que receba dois numeros e um operador (+, -, *, /) e retorne o resultado da operacao.
+Exemplo:
+Digite o primeiro numero: 5
+Digite o segundo numero: 2
+Digite a operacao (+, -, *, /): *
+Resultado: 10
+*/
+fun lerDadosDigitados() {
+    println("Digite o primeiro número: ")
+    val primeiroNumero: Double = readLine().toString().trim().toDouble()
 
-  calcular(primeiroNumero, segundoNumero, operador) 
+    println("Digite o segundo número: ")
+    val segundoNumero: Double = readLine().toString().trim().toDouble()
+
+    println("Digite a operação (+, -, *, /): ")
+    val operador: String = readLine().toString().trim()
+
+    val resultado = calcular(primeiroNumero, segundoNumero, operador)
+    println("Resultado: $resultado")
 }
 
-fun calcular(primeiroNumero: Double, segundoNumero: Double, operador:String): Double?{ 
-  val resultado = when (operador) { 
-    "+" -> primeiroNumero + segundoNumero 
-    "-" -> primeiroNumero - segundoNumero 
-    "*" -> primeiroNumero * segundoNumero 
-    "/" -> { 
-      if (segundoNumero != 0.0) { 
-      primeiroNumero/segundoNumero 
-      } else { 
-        println("Erro: Divisão por zero não é permitida.")
-        null 
-      } 
-    } 
-    else -> { 
-      println("Erro: Operador inválido.") null 
-    } 
-  } 
-  return resultado 
+fun calcular(primeiroNumero: Double, segundoNumero: Double, operador:String): String{
+    return when (operador) {
+        "+" -> (primeiroNumero + segundoNumero).toString()
+        "-" -> (primeiroNumero - segundoNumero).toString()
+        "*" -> (primeiroNumero * segundoNumero).toString()
+        "/" -> {
+            if (segundoNumero != 0.0) {
+                (primeiroNumero/segundoNumero).toString()
+            } else {
+                "Erro: Divisão por zero não é permitida."
+            }
+        }
+        else -> "Erro: Operador inválido."
+    }
 }
 
-fun imprimirResultado(){ 
-  println("Resultado: $resultado") 
-} 
-
+fun main(){
+    lerDadosDigitados()
+}
 fun main(){ 
   imprimirResultado() 
 }
