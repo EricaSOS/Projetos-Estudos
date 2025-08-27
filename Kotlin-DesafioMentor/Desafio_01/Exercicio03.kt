fdb49d0e1f1baba29601360c0848a11d4656d878
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-08-27 08:56:10 -0300

    feat: Update Exercicio03.kt, com funções lerDadosDigitados, calcular, imprimirResultado e a main

--- Kotlin-DesafioMentor/Desafio_01/Exercicio03.kt
+++ Kotlin-DesafioMentor/Desafio_01/Exercicio03.kt
@@ -1,1 +1,40 @@
+fun lerDadosDigitados() { 
+  println("Digite o primeiro número: ") 
+  val primeiroNumero: Double = readLine().toString().trim().toDouble()
+  
+  println("Digite o segundo número: ") 
+  val segundoNumero: Double = readLine().toString().trim().toDouble() 
+  
+  println("Digite a operação (+, -, *, /): ") 
+  val operador: String = readLine().toString().trim() 
 
+  calcular(primeiroNumero, segundoNumero, operador) 
+}
+
+fun calcular(primeiroNumero: Double, segundoNumero: Double, operador:String): Double?{ 
+  val resultado = when (operador) { 
+    "+" -> primeiroNumero + segundoNumero 
+    "-" -> primeiroNumero - segundoNumero 
+    "*" -> primeiroNumero * segundoNumero 
+    "/" -> { 
+      if (segundoNumero != 0.0) { 
+      primeiroNumero/segundoNumero 
+      } else { 
+        println("Erro: Divisão por zero não é permitida.")
+        null 
+      } 
+    } 
+    else -> { 
+      println("Erro: Operador inválido.") null 
+    } 
+  } 
+  return resultado 
+}
+
+fun imprimirResultado(){ 
+  println("Resultado: $resultado") 
+} 
+
+fun main(){ 
+  imprimirResultado() 
+}
