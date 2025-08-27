fb274b5fc67e760f669ddfb0cab120bb7aa5d3ad
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-08-27 17:11:33 -0300

    feat: inclusão do comando da questão Update Exercicio05.kt

--- Kotlin-DesafioMentor/Desafio_01/Exercicio05.kt
+++ Kotlin-DesafioMentor/Desafio_01/Exercicio05.kt
@@ -1,1 +1,10 @@
-
+/*
+Exercicio 5 - Classificacao por Idade
+Crie um programa que leia a idade de uma pessoa e classifique como:
+- "Menor de idade" se for menor que 18
+- "Adulto" se estiver entre 18 e 59
+- "Idoso" se tiver 60 ou mais
+Exemplo:
+Digite sua idade: 45
+Resultado: Adulto
+*/
