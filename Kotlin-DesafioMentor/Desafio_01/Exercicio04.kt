5002ddfde060fce3c701e27a5a338e577aceb3da
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-08-27 16:59:46 -0300

    feat: Update Exercicio04.kt inseridos comando e resolução do Exercício 4

--- Kotlin-DesafioMentor/Desafio_01/Exercicio04.kt
+++ Kotlin-DesafioMentor/Desafio_01/Exercicio04.kt
@@ -1,1 +1,35 @@
+/*
+Exercicio 4 - Validador de Senha
+Crie uma funcao que valide uma senha com base nas seguintes regras:
+- Ter pelo menos 8 caracteres
+- Conter pelo menos uma letra maiuscula
+- Conter pelo menos um numero
+- Nao conter espacos
+Exemplo de retorno:
+Digite a senha: MinhaSenha123
+Senha valida: true
+*/
 
+fun validarSenha(senha: String){
+    var tamanhoSenha: Int = senha.length
+    var contemLetraMaiuscula: Boolean= senha.any{it.isUpperCase()}
+    var contadorNumero: Boolean = senha.any{it.isDigit()}
+    var contemEspaco: Boolean = senha.contains(" ")
+
+
+    if ((tamanhoSenha >=8) && (contemLetraMaiuscula === true) && (contadorNumero === true) && (contemEspaco === false)){
+         println("Senha valida: true")
+    } else {
+        println("Senha valida: false")
+    }
+}
+
+fun receberSenha(){
+    print("Digite a senha: ")
+    var senha = readln().toString()
+    validarSenha(senha)
+}
+
+fun main(){
+    receberSenha()
+}
