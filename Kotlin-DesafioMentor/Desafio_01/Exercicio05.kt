24555f58742b4367afbbdf5802a04b13cf017037
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-08-27 21:08:14 -0300

    feat: Update Exercicio05.kt, inserida a resolução

--- Kotlin-DesafioMentor/Desafio_01/Exercicio05.kt
+++ Kotlin-DesafioMentor/Desafio_01/Exercicio05.kt
@@ -8,3 +8,23 @@ Exemplo:
 Digite sua idade: 45
 Resultado: Adulto
 */
+fun lerDadoDigitado(){
+    print("Digite sua idade: ")
+    var idadeUsuario = readln().trim().toInt()
+
+    classificadorEtario(idadeUsuario)
+}
+
+fun classificadorEtario(idadeUsuario: Int){
+    if(idadeUsuario < 18){
+        print("Resultado: Menor de idade")
+    } else if(idadeUsuario >= 18 && idadeUsuario <= 59){
+        print("Resultado: Adulto")
+    } else {
+        print("Resultado: Idoso")
+    }
+}
+
+fun main(){
+    lerDadoDigitado()
+}
