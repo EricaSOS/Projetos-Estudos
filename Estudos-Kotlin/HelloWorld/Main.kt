352a2d736b92d53e6b3bbf0040b70003d926172c
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-12-09 15:50:33 -0300

    Create Main.kt

---
+++ Estudos-Kotlin/HelloWorld/Main.kt
@@ -0,0 +1,11 @@
+/**
+ * Comentário do tipo Documentação
+ */
+//Comentário do tipo linha única: Função Exercicios.main é um ponto de entrada, só pode ter uma por arquivo.
+/* Comentário do tipo linhas multiplas:
+No momento da compilação, os comentários são excluídos, então, não há
+uma preocupação de que com comentários o código fica pesado */
+fun main() {
+    var teste: String = "Hello World!"
+    println(teste)
+}
