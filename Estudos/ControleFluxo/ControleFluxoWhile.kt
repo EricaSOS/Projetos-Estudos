e5a184e7a50c477fd08957fbc853ee85a95c5626
Erica Santos <109611231+EricaSOS@users.noreply.github.com>
2025-12-09 15:56:36 -0300

    Delete Estudos directory

--- Estudos/ControleFluxo/ControleFluxoWhile.kt
+++
@@ -1,8 +0,0 @@
-/* A sintaxe é: Enquanto uma condição é verdadeira, eu executo determinada instrução
-    while (condição V ou F){
-    instrução
-    precisa de algo para alterar a condição e não gerar loop
-    }
-
-    tbm são aplicados break e continue
- */
